f662be47e425c77a2903ddc0600c4e4e3b22f370
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-03 20:03:24 +0530

    Add files via upload

---
+++ prog43.cpp
@@ -0,0 +1,55 @@
+ //program to understand the concept of copy constructor!!!
+#include<iostream>
+#include<string.h>
+using namespace std;
+class student{
+        int roll_no;
+        char name[50];
+        double per;
+    
+public:
+    
+    student(){
+        cout<<"default constructor called " <<endl ;
+    }
+
+    student(int roll_no,char name[],double per)
+    {
+       this->roll_no=roll_no;
+       getchar();
+       strcpy(this->name,name);
+       this->per=per;
+       cout<<"parameterized constructor called"<<endl;
+    }    
+        void displayDetail(){
+       cout<<"roll no:"<<roll_no<<endl;
+        cout<<"name:"<<name<<endl;
+        cout<<"percentage:"<<per<<endl;
+        
+        }
+
+};
+int main()
+{    
+        int roll_no;
+        char name[50];
+        double per;
+        cout<<"enter roll:"<<endl;
+        cin>>roll_no;
+        cout<<"enter name:"<<endl;
+        getchar();
+        cin.getline(name,50);
+        cout<<"enter percentage:"<<endl;
+        cin>>per;
+       
+       student obj(roll_no,name,per) , obj1(obj);
+       obj.displayDetail();
+
+       cout<<"-----after copying------"<<endl ;
+       obj1.displayDetail();
+
+  
+
+  return 0;
+
+}
