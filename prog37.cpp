e0f6c13ed58d8e53bdda9b452a8e4603f58b738d
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-03 19:55:51 +0530

    Add files via upload

---
+++ prog37.cpp
@@ -0,0 +1,20 @@
+//program to understand the concept of constructor!!!
+#include<iostream>
+using namespace std;
+class demo{
+
+public:
+    demo(){
+        cout<<"default constructor call"<<endl;
+    }
+   
+};
+int main()
+{
+  demo obj1,obj2,ob3;
+  return 0;
+
+}
+
+
+
