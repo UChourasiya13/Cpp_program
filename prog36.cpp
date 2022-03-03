ea21042f0c62bfc531ed618ebbd244df9bf9c9bb
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-03 19:55:06 +0530

    Add files via upload

---
+++ prog36.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+  string str;
+  cout<<"enter name"<<endl;
+  getline(cin,str);
+  cout<<str;
+  return 0;
+
+
+}
+    
+
+
