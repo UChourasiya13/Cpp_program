57752ea4948d0a95b132e5269170472af2cda64d
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-04 21:11:06 +0530

    Add files via upload

---
+++ prog46.cpp
@@ -0,0 +1,66 @@
+ //program to understand the concept of passing object in a function!!!
+#include<iostream>
+using namespace std;
+
+class Distance {
+
+    private  :
+
+    int km , m ;
+
+    public :
+
+   void setDistance( int km , int m ){
+      
+      this->km = km ;
+      this->m = m ;
+      }
+   void display( ){
+       cout<< "\t"<<km <<"\t"<<m<<endl;
+   }   
+
+  Distance addDistance( Distance d2 ){
+     
+     Distance obj ; 
+    obj.km = km +d2.km ;
+    obj.m = m + d2.m ;
+
+    while(obj.m>= 100){
+        obj.m-=1000;
+        obj.km += 1 ;
+    }
+
+    return obj ;
+
+  }
+};
+
+int main(){
+Distance d1 , d2 , d3;
+
+int km1 , km2 , m1 , m2 ;
+
+cout<<"enter km and m "<<endl;
+cin>>km1>>m1 ;
+cout<<"enter km and  m again "<<endl;
+cin>>km2>>m2 ;
+
+d1.setDistance( km1 , m1) ;
+d2.setDistance( km2 , m2) ;
+
+cout<<"----------------------"<<endl;
+cout<<"\t km   \t m "<<endl;
+cout<<"----------------------"<<endl;
+
+d1.display();
+d2.display();
+
+cout<<"----------------------"<<endl;
+d1.addDistance(d2) ;
+d3 =   d1.addDistance(d2) ;
+d3.display() ;
+
+return 0 ;
+
+}
+
