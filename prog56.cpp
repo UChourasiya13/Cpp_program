c2f42d7f39d963bff2c5d2e486640502cb4a0afd
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-06 19:19:51 +0530

    Add files via upload

---
+++ prog56.cpp
@@ -0,0 +1,71 @@
+//program to understand the concept of (binary)operator overloading
+
+#include <iostream>
+using namespace std;
+
+class Distance {
+
+    private  :
+
+    int km , m ;
+
+    public :
+
+   void setDistance( int km , int m ){
+      
+      this->km = km ;
+      this->m = m ;
+      }
+   void display( ){
+       cout<< "\t"<<km <<"\t"<<m<<endl;
+   }
+
+  friend Distance operator + ( Distance d1, Distance d2);
+  
+};
+
+Distance operator+(Distance d1, Distance d2)
+{
+
+    Distance obj;
+    obj.km = d1.km + d2.km;
+    obj.m = d1.m + d2.m;
+
+    while (obj.m >= 100)
+    {
+        obj.m -= 1000;
+        obj.km += 1;
+    }
+
+    return obj;
+}
+
+int main(){
+Distance d1 , d2 , d3;
+
+int km1 , km2 , m1 , m2 ;
+
+cout<<"enter km and m "<<endl;
+cin>>km1>>m1 ;
+cout<<"enter km and  m again "<<endl;
+cin>>km2>>m2 ;
+
+d1.setDistance( km1 , m1) ;
+d2.setDistance( km2 , m2) ;
+
+cout<<"----------------------"<<endl;
+cout<<"\t km   \t m "<<endl;
+cout<<"----------------------"<<endl;
+
+d1.display();
+d2.display();
+
+cout<<"----------------------"<<endl;
+
+d3 = d1 + d2;
+d3.display() ;
+
+return 0 ;
+
+}
+ 
